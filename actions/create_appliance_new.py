48a991091fd7dbfbdf9c7f3d32c771f7aa1099f8
N.G <ameliang96@live.com.au>
2018-05-08 10:43:28 +1000

    APPLIANCE - Added possible new way to implement create appliance

---
+++ actions/create_appliance_new.py
@@ -0,0 +1,58 @@
+import logging
+from flask import json
+from requests.auth import HTTPBasicAuth
+import requests
+from util import get_site_id_by_name
+
+# name, location and city, country 
+# Create panda shadow appliance for banana in Texas, US
+# create 
+
+def create_appliance_new(api_auth, parameters, contexts):
+    """
+    :param api_auth: SteelConnect api object
+    :type api_auth: SteelConnectAPI
+    :param parameters: json parameters from Dialogflow intent
+    :type parameters: json
+    :return: Returns a response to be read out to user
+    :rtype: string
+    """
+    try:
+        city = parameters["City"].replace(" ", "")  # .replace() is for locations where there are spaces. E.g. Kuala Lumpur
+        site_name = parameters["SiteName"]
+        model = parameters["Model"]
+        country_code = parameters["Country"]["alpha-2"]
+ 
+    except KeyError as e:
+        error_string = "Error processing create Appliance intent. {0}".format(e)
+        logging.error(error_string)
+        return error_string
+
+    # Get all sites and check whether site exists
+    site_id = get_site_id_by_name(api_auth, site_name, city,country_code)
+        
+    data_sites = api_auth.site.list_sites().json()
+    for item in data_sites["items"]:
+        if item["id"] == site_id:
+            break
+
+    if site_id:
+        # Call create_appliance in SteelConnectAPI
+        res = api_auth.node.create_appliance(site=site_id, model=model)
+ 
+        if res.status_code == 200:
+            speech = "Appliance: {} created for site: {}, {}".format(model, city, site_name)
+        elif res.status_code == 400:
+            speech = "Invalid parameters: {}".format(res.json()["error"]["message"])
+        elif res.status_code == 404:
+            speech = "Error: Organization with given id does not exist"
+        elif res.status_code == 500:
+            speech = "Error: Could not create Appliance"
+        else:
+            speech = "Error: Could not connect to SteelConnect"
+ 
+        logging.debug(speech)
+    else:
+        speech = "Invalid site {} in {}".format(site_name, city)
+    return speech
+
