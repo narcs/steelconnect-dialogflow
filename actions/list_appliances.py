e4b78a4bb63e2bb8497fe7f6db799b0ab12d647b
N.G <ameliang96@live.com.au>
2018-05-11 22:07:48 +1000

    APPLIANCE - Updated list appliance message

--- actions/list_appliances.py
+++ actions/list_appliances.py
@@ -7,20 +7,22 @@ from requests.auth import HTTPBasicAuth
 def list_appliances(api_auth, parameters, contexts):
  
     logging.info("Listing Appliances")
-    org = parameters["organisation"]
  
     res = api_auth.node.list_appliances()
 
     if res.status_code == 200:
         data = res.json()["items"]
         num_appliances = len(data)
- 
+        appliance_list = []
+        for appliance in data:
+            appliance_list.append(appliance["model"] + " appliance (ID: " + appliance["id"] + ") on  site ID " + appliance["site"] + "\n")
+
         if num_appliances == 0:
-            speech = "There are no appliances in the {} organisation".format(org)
+            speech = "There are no appliances"
         elif num_appliances == 1:
-            speech = "There is one appliance in the {} organisation, it is a {} on {} ".format(org, data[0]["model"], data[0]["site"] )
+            speech = "There is only one appliance, it is a {} on {} ".format(data[0]["model"], data[0]["site"] )
         elif num_appliances > 1:
-            speech = "There are {} appliances in the {} organisation, would you like to list all of them?".format(num_appliances, org)
+            speech = "There are {} appliances. \n{}".format(num_appliances, "".join(appliance_list))
         else:
             speech = "Unknown error occurred when retrieving appliances"
     else:
