16015e0e07fadfcb2bfd95f18da6c03a4771c3b1
Andrew Abbott <mirrexagon@mirrexagon.com>
2018-07-26 15:13:21 +1000

    Finish impl of sitelinks report

--- actions/get_sitelinks_report.py
+++ actions/get_sitelinks_report.py
@@ -1,4 +1,5 @@
 import logging
+from collections import Counter
 
 from flask import json
 from actions.util import *
@@ -13,16 +14,21 @@ def get_sitelinks_report(api_auth, parameters, contexts):
     :rtype: string
     """
 
-    all_sitelinks = {}
+    # It seems that sitelinks have different IDs depending on which side
+    # you're on. So all sitelinks are counted twice.
+    all_sitelinks = []
     error_occurred = False
 
     # To check every sitelink, we need to go through each site in the org
     # and compile a list of all unique sitelinks, then count statuses.
+    # I haven't seen a way to just get a list of all sitelinks in an org.
     sites_res = api_auth.site.list_sites()
     if sites_res.status_code != 200:
         return "Failed to get list of sites for getting all sitelinks"
 
-    for site in sites_data.json()["items"]:
+    for site in sites_res.json()["items"]:
+        logging.info("Checking site {}".format(site["id"]))
+
         sitelinks_res = api_auth.sitelink.get_sitelinks(site["id"])
         if sitelinks_res.status_code == 404:
             pass # No sitelinks for this site
@@ -32,9 +38,21 @@ def get_sitelinks_report(api_auth, parameters, contexts):
             error_occurred = True
         else:
             for sitelink in sitelinks_res.json()["items"]:
-                if sitelink["id"] not in all_sitelinks:
-                    all_sitelinks[sitelink["id"]] = sitelink
+                logging.info("  Sitelink: {} | {} -> {} | {}".format(sitelink["id"],
+                    site["id"], sitelink["remote_site"], sitelink["status"]))
+                all_sitelinks.append(sitelink)
 
     # Now let us summarise these sitelinks.
-    return "There are {} sitelinks".format(len(all_sitelinks))
+    # Because I don't know all the statuses, I'm just going to count what I see.
+    statuses = Counter()
+
+    for sitelink in all_sitelinks:
+        statuses[sitelink["status"]] += 1
+
+    speech = "There are {} sitelinks".format(len(all_sitelinks))
+
+    for status in statuses:
+        speech += "\n  {} {}".format(statuses[status], status)
+
+    return speech
 
