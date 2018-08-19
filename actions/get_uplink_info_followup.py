ea71581f14a0b592c5f207994af2da0a06235579
Unknown <ameliang96@live.com.au>
2018-08-19 18:17:49 +1000

    Added a follow up to facilitate the rewrite of the reporting function

---
+++ actions/get_uplink_info_followup.py
@@ -0,0 +1,21 @@
+import logging
+from flask import json
+
+def get_uplink_info_followup(api_auth, parameters, contexts):
+    try: 
+        option_choice = int(parameters["OptionNumber"])
+    except KeyError as e:
+        error_string = "Error processing getting uplink information followup"
+        logging.error(error_string)
+        return error_string
+
+    uplinks_res = api_auth.uplink.get_uplink_info()
+    if uplinks_res.status_code != 200:
+        return "Failed to get a list of uplinks"
+    
+    uplinks = uplinks_res.json()["items"]
+    uplink_information = uplinks[option_choice - 1]
+    information = api_auth.uplink.format_uplink_information(uplink_information)
+    speech = "Information for Uplink {}: \n{}".format(uplinks[option_choice - 1]["id"], information)
+
+    return speech
