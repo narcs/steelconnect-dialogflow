205f666b435ebd69724e9b7a7a4d4a265d7ee094
Unknown <ameliang96@live.com.au>
2018-10-05 18:20:25 +1000

    Added documentation for delete sites

--- actions/delete_site.py
+++ actions/delete_site.py
@@ -1,16 +1,31 @@
 import logging
-
 from flask import json
-from actions.util import *
+from actions.util import get_site_id_by_name, APIError
 
 def delete_site(api_auth, parameters, contexts):
     """
-    :param api_auth: steelconnect api object
-    :type api_auth: SteelConnectAPI
-    :param parameters: json parameters from Dialogflow intent
-    :type parameters: json
-    :return: Returns a response to be read out to user
-    :rtype: string
+    Allows users to delete a site 
+    In order for them to do so, we need to know the city, site name and country code. 
+
+    Works by getting the parameters, and calling the SteelConnect API with the parameters to delete
+    the site. It checks to see if the site does exist, and if it does, it will delete the site. It
+    not, it will let the user know that the site doesn't exist
+
+    Parameters:
+    - api_auth: SteelConnect API object, it contains authentication log in details
+    - parameters: The json parameters obtained from the Dialogflow Intent. It obtains the following:
+        > city: The city where the user wants to delete the site
+        > country_code: The country code of where the user wants to delete the site
+            + The country name will be obtained from the country code, and will not be directly
+              retrieved from the user
+        > site_name: A name that the user gives the site they want to delete 
+    
+    Returns:
+    - speech: A string which has the response to be read/printed to the user
+
+    Example Prompt:
+    - delete a site called bulbasaur in Hobart, Australia
+
     """
     
     try:
@@ -20,11 +35,8 @@ def delete_site(api_auth, parameters, contexts):
         country_name = parameters["Country"]["name"]
         
     except KeyError as e:
-
         error_string = "Error processing deleteSite intent. {0}".format(e)
-
         logging.error(error_string)
-
         return error_string
 
     # Grab site id by name, city and country
