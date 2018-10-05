acb59c8a3312d751d6aad8b9d8881eb38b08967f
Unknown <ameliang96@live.com.au>
2018-10-05 19:15:55 +1000

    Updated formatting and documentation for list uplinks

--- actions/uplink.py
+++ actions/uplink.py
@@ -1,13 +1,23 @@
 import logging
-
 from flask import json
 
 def list_uplinks(api_auth, parameters, contexts):
     """
-    :param parameters: json parameters from Dialogflow intent
-    :type parameters: json
-    :return: Returns a response to be read out to user
-    :rtype: string
+    Allows users to list all the uplinks that are in the organisation and get a brief overview
+
+    Works by calling the list_uplinks action defined in api/uplink.py
+
+    Parameters:
+    - api_auth: SteelConnect API object, it contains authentication log in details
+    - parameters: The json parameters obtained from the Dialogflow Intent. In this case, we
+                  obtain nothing
+
+    Returns:
+    - speech: A string which has the list of all uplinks in the organisation
+
+    Example Prompt:
+    - List uplinks
+
     """
     
     # Get all uplinks and return a response based on the number of uplinks
@@ -20,7 +30,7 @@ def list_uplinks(api_auth, parameters, contexts):
         if num_uplinks == 0:
             speech = "There are no uplinks"
         elif num_uplinks >= 1:
-            speech = "All uplinks:\n"
+            speech = "There are {} uplinks in the organisation:\n".format(num_uplinks)
             count = 1
             for uplink in data:
                 site = api_auth.site.get_site(uplink["site"])
@@ -29,15 +39,13 @@ def list_uplinks(api_auth, parameters, contexts):
                 wan = api_auth.wan.get_wan(uplink["wan"])
                 wan_name = wan.json()["longname"]
 
-                speech += "\n {}. {}/{}/{}".format(count, site_name, name, wan_name)
+                #added leading zeroes in the count variable to help with formatting of data
+                speech += "\n{}. ID: {}\n\t  Site: {}\n\t  Uplink Name: {}\n\t  Connected WAN: {}\n".format(str(count).zfill(2), uplink["id"], site_name, name, wan_name)
                 count += 1
-
         else:
             speech = "Unknown error occurred when retrieving uplinks"
-
     else:
         speech = "Error: Could not connect to SteelConnect"
 
     logging.debug(speech)
-
     return speech
