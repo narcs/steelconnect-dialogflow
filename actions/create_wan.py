0b959ffe60bd740ccf174faf5bd6e186284766df
Unknown <ameliang96@live.com.au>
2018-10-05 17:39:06 +1000

    Clean up code  and added documentation for create WAN

--- actions/create_wan.py
+++ actions/create_wan.py
@@ -4,35 +4,40 @@ from flask import json
 
 def create_wan(api_auth, parameters, contexts):
     """
-    :param api_auth: steelconnect api object
-    :type api_auth: SteelConnectAPI
-    :param parameters: json parameters from Dialogflow intent
-    :type parameters: json
-    :param contexts: json contexts from Dialogflow intent
-    :type parameters: json
-    :return: Returns a response to be read out to user
-    :rtype: string
+    Allows users to create a WAN 
+    In order for them to do so, we need to know the WAN type and a name for the WAN
+
+    Works by getting the parameters, and calling the SteelConnect API with the parameters to create
+    the WAN
+
+    Parameters:
+    - api_auth: SteelConnect API object, it contains authentication log in details
+    - parameters: The json parameters obtained from the Dialogflow Intent. It obtains the following:
+        > WAN_Name: A name/label the user gives for the WAN they create
+
+    Returns:
+    - speech: A string which has the response to be read/printed to the user
+
+    Example Prompt:
+    - Create a WAN named FireStone
+
     """
     try:
-        WAN_type = parameters["WANType"]
-        name = WAN_type
+        WAN_Name = parameters["WANType"]       
 
     except KeyError as e:
-
         error_string = "Error processing createWAN intent. {0}".format(e)
-
         logging.error(error_string)
-
         return error_string
 
-    res = api_auth.wan.create_wan(name)
+    res = api_auth.wan.create_wan(WAN_Name)         #Calls SteelConnectAPI and creates the WAN
 
     if res.status_code == 200:
-        speech = "A WAN called {} was created".format(WAN_type)
+        speech = "A WAN called {} was created".format(WAN_Name)
     elif res.status_code == 400:
         speech = "Invalid parameters: {}".format(res.json()["error"]["message"])
     elif res.status_code == 500:
-        speech = "Error: Could not create WAN"
+        speech = "Error: Could not create WAN {}".format(WAN_Name)
     else:
         speech = "Error: Could not connect to SteelConnect"
 
