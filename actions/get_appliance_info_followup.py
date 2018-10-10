ffdca916e9e9559d3a3fe4f1f672b904bba6efdc
Unknown <ameliang96@live.com.au>
2018-10-10 11:58:25 +1100

    Added error checking if user inputs a number not in a valid range

--- actions/get_appliance_info_followup.py
+++ actions/get_appliance_info_followup.py
@@ -11,7 +11,8 @@ def get_appliance_info_followup(api_auth, parameters, contexts):
 
     Works by retrieving the list of appliances that match the model and site that the user requests
     to get detailed information about. It asks the user for an option number, and delete the 
-    appliance that matches the option number the user put in. 
+    appliance that matches the option number the user put in. A check is done to make sure it is 
+    within range
 
     Parameters:
     - api_auth: SteelConnect API object, it contains authentication log in details
@@ -19,7 +20,7 @@ def get_appliance_info_followup(api_auth, parameters, contexts):
         > option_choice: An integer that references an option to delete the appliance
 
     Returns:
-    - speech: A string which has the list of all sites in the organisation
+    - speech: A string which has the response to be read/printed to the user
 
     Example Prompt:
     - 2
@@ -36,9 +37,12 @@ def get_appliance_info_followup(api_auth, parameters, contexts):
     appliance_id = appliance_options[option_choice - 1]     #option_choice -1 because arrays start at zero <-- this is more of a reminder sort of thing...
     res = api_auth.node.get_appliance_info(node_id= appliance_id)
     if res.status_code == 200:
-        appliance_info = res.json()
-        information = api_auth.node.format_appliance_information(appliance_info)
-        speech = "Information for Appliance ID: {}\n{}".format(appliance_id, "".join(information))
+        if option_choice < 1 or option_choice > len(appliance_options):        #Check to see if the value the user inputted is within range
+            speech = "Please selected a number between 1 and {}".format(len(appliance_options))
+        else:
+            appliance_info = res.json()
+            information = api_auth.node.format_appliance_information(appliance_info)
+            speech = "Information for Appliance ID: {}\n{}".format(appliance_id, "".join(information))
     else:
         return "Error: Failed to get information for Appliance {}".format(appliance_id)
     return speech    
